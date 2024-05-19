94b6be97bb2feee21b7f756f625ca7a39cdd3240
Chaicoo <gabriellimma27@gmail.com>
2024-05-19 18:16:44 -0300

    feat: add components shadcn

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,4 +1,4 @@
-import { GithubIcon, LinkedinIcon, MailIcon } from "lucide-react";
+import { GithubIcon, LinkedinIcon } from "lucide-react";
 import { ModeToggle } from "./mode-theme-togle";
 
 export function Header() {
@@ -6,20 +6,13 @@ export function Header() {
         <header className='sticky top-0 px-5 md:px-32 backdrop-blur-md border-b border-gray-700 border-opacity-50 dark:border-opacity-20 z-20'>
             <div className='flex h-20 items-center justify-center relative'>
                 <span
-                    className='sm:text-2xl font-bold mx-auto text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 dark:via-sky-400 dark:to-blue-400'
+                    className='sm:text-2xl font-bold mx-auto'
                 >
-                    Francisco Nolêto
+                    CRUD Nest.Js
                 </span>
                 <div className="absolute right-0 top-1/2 transform -translate-y-1/2">
                     <div className='hidden md:flex  gap-2 items-center'>
-                        <a
-                            href='mailto:franciscolima.dev@gmail.com'
-                            target='_blank'
-                            rel='noreferrer'
-                            className='rounded-md p-2.5 hover:bg-accent transition-colors ease-out'
-                        >
-                            <MailIcon className='h-4 w-4 text-black dark:text-white' />
-                        </a>
+
                         <a
                             href='https://github.com/Chaicoo'
                             target='_blank'
